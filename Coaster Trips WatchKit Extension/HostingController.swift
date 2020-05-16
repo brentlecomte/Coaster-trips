9fc03aa9bad4618d998b03f8bb3c2e1532de2ab4
Brent Le Comte <brent.lecomte@inthepocket.mobi>
2020-05-16 18:57:08 +0200

    Initial Commit

---
+++ Coaster Trips WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Coaster Trips WatchKit Extension
+//
+//  Created by Brent Le Comte on 16/05/2020.
+//  Copyright © 2020 Brent Le Comte. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Coaster Trips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coaster Trips
+//
+//  Created by Brent Le Comte on 16/05/2020.
+//  Copyright © 2020 Brent Le Comte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
